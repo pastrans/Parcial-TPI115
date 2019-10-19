457f7fb9755ad8aa5a2af58198758a24bfec2a93
unknown <publicponce@outlook.com>
2019-10-19 13:35:56 -0500

    cambios menores de estilo

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,3 @@
-
 function myFunction() {
     var x = document.getElementById("myTop");
     if (x.className === "topnav") {
@@ -37,3 +36,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+/*var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}*/
